a43aa10c18b96183e6e267c4654a0e3f29443af2
MostafaGhidanM <mostafaghidannti26nov@gmail.com>
2025-09-16 02:46:57 +0300

    fix

--- controllers/website.py
+++ controllers/website.py
@@ -11,5 +11,33 @@ _logger = logging.getLogger(__name__)
 class TechDreamWebsite(Website):
     """Enhanced website controller for TechDream theme"""
 
-    # Remove custom homepage route to avoid conflicts - use Odoo's default
-    pass
+    # Basic routes that should work
+    @http.route(['/shop/smartphones', '/shop/category/smartphones'], type='http', auth="public", website=True)
+    def smartphones(self, **kw):
+        """Smartphones category page"""
+        return request.redirect('/shop')
+
+    @http.route(['/shop/accessories', '/shop/category/accessories'], type='http', auth="public", website=True)
+    def accessories(self, **kw):
+        """Accessories category page"""
+        return request.redirect('/shop')
+
+    @http.route(['/shop/audio', '/shop/category/audio'], type='http', auth="public", website=True)
+    def audio(self, **kw):
+        """Audio category page"""
+        return request.redirect('/shop')
+
+    @http.route(['/shop/smartwatches', '/shop/category/smartwatches'], type='http', auth="public", website=True)
+    def smartwatches(self, **kw):
+        """Smartwatches category page"""
+        return request.redirect('/shop')
+
+    @http.route(['/shop/offers', '/shop/category/offers'], type='http', auth="public", website=True)
+    def offers(self, **kw):
+        """Special offers page"""
+        return request.redirect('/shop')
+
+    @http.route('/contact', type='http', auth="public", website=True)
+    def contact_us(self, **kw):
+        """Contact us page"""
+        return request.redirect('/contactus')
