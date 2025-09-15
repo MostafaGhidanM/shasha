5ac41cbc025154a82e1dcd996477264a0dd17b12
root <root@ip-172-31-27-117.eu-north-1.compute.internal>
2025-09-15 16:12:40 +0000

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'TechDream Mobile Theme',
+    'version': '1.0.0',
+    'category': 'Theme/eCommerce',
+    'summary': 'Custom theme for TechDream Mobile',
+    'depends': ['website', 'website_sale'],
+    'data': [
+        'views/theme.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
