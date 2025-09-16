95f1db38d8fbd3f5530f8deae3fde9001545138c
MostafaGhidanM <mostafaghidannti26nov@gmail.com>
2025-09-16 15:04:47 +0300

    fix 2

--- models/product_wishlist.py
+++ models/product_wishlist.py
@@ -8,27 +8,30 @@ class ProductWishlist(models.Model):
     _description = 'Product Wishlist'
     _rec_name = 'product_id'
 
-    user_id = fields.Many2one('res.users', string='User', required=True, default=lambda self: self.env.user)
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True,
+                                default=lambda self: self.env.user.partner_id)
     product_id = fields.Many2one('product.template', string='Product', required=True)
     date_added = fields.Datetime('Date Added', default=fields.Datetime.now)
 
     _sql_constraints = [
-        ('unique_user_product', 'unique(user_id, product_id)', 'Product already in wishlist!')
+        ('unique_partner_product', 'unique(partner_id, product_id)', 'Product already in wishlist!')
     ]
 
     @api.model
     def toggle_wishlist(self, product_id):
-        existing = self.search([('user_id', '=', self.env.user.id), ('product_id', '=', product_id)])
+        partner_id = self.env.user.partner_id.id
+        existing = self.search([('partner_id', '=', partner_id), ('product_id', '=', product_id)])
         if existing:
             existing.unlink()
             return {'in_wishlist': False}
         else:
-            self.create({'product_id': product_id})
+            self.create({'product_id': product_id, 'partner_id': partner_id})
             return {'in_wishlist': True}
 
     @api.model
     def is_in_wishlist(self, product_id):
-        return bool(self.search([('user_id', '=', self.env.user.id), ('product_id', '=', product_id)]))
+        partner_id = self.env.user.partner_id.id
+        return bool(self.search([('partner_id', '=', partner_id), ('product_id', '=', product_id)]))
 
 
 class ProductTemplate(models.Model):
@@ -36,7 +39,8 @@ class ProductTemplate(models.Model):
 
     @api.model
     def get_wishlist_products(self, limit=20):
+        partner_id = self.env.user.partner_id.id
         wishlist_items = self.env['product.wishlist'].search([
-            ('user_id', '=', self.env.user.id)
+            ('partner_id', '=', partner_id)
         ], limit=limit, order='date_added desc')
         return wishlist_items.product_id
